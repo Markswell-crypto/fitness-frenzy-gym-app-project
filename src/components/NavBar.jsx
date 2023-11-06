769642fe15ebcbbdca2634864b027ee387d50d17
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-11-06 21:15:19 -0100

    created navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default NavBar
+    
+    <nav className="navbar-list">
+    
+      <ul className="navbar-list">
+    <li><a href="/">HOME</a></li>
+    <li><a href="/about">ABOUT</a></li>
+    <li><a href="/exercises">EXERCISES</a></li>
+    <li><a href="/contact">CONTACT</a></li>
+    <li><a href="/reviews">REVIEWS</a></li>
+  </ul>
+
+    </nav>
+  );
+};
+
+export default NavBar;
