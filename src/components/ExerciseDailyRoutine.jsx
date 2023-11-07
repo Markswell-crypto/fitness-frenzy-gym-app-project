2c05051ac3d532c80a31032d123859079f36d2bd
Gerald-GG <gicharugerald@gmail.com>
2023-11-07 20:29:13 +0300

    Add  component for managing daily exercise routines

--- src/components/ExerciseDailyRoutine.jsx
+++ src/components/ExerciseDailyRoutine.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 function ExerciseDailyRoutine({ exercises }) {
-    const [dailyRoutine, setDailyRoutine] = useState([]);
-
+    const [dailyRoutine, setDailyRoutine] = useState([]);  //state to manage the daily exercise routine
+    
+    // function to add an exercise to the daily routine
     const handleAddToRoutine = (exercise) => {
+        // creating a copy of current dailyroutine & add selected exercise with default sets and repetitions
         const updatedRoutine = [...dailyRoutine, {exercise, set: 0, repetitions: 0 }];
         setDailyRoutine(updatedRoutine);
     };
 
+    // function to handle changes in sets and repetitions for a specific exercise in the daily routine
     const handleSetsRepetitionsChange = (index, field, value) => {
+        // creating a copy of the current dailyroutine and update sets or repetitions value for selected exercise
         const updatedRoutine = [...dailyRoutine];
         updatedRoutine[index][field] = value;
         setDailyRoutine(updatedRoutine);
@@ -20,6 +24,7 @@ function ExerciseDailyRoutine({ exercises }) {
             {exercises.map((exercise, index) => (
                 <div key={exercise.id}>
                     <h3>{exercise.name}</h3>
+                    {/* button to add exercise to daily routine */}
                     <button onClick={() => handleAddToRoutine(exercise)}>Add to Routine</button>
                     {dailyRoutine.map((item, itemIndex) => {
                         if (item.exercise.id === exercise.id) {
@@ -29,7 +34,7 @@ function ExerciseDailyRoutine({ exercises }) {
                                     <input 
                                       type="number"
                                       value={item.set}
-                                      onChange={(e) => handleSetsRepetitionsChange(itemIndex, 'sets', e.target.value)}
+                                      onChange={(e) => handleSetsRepetitionsChange(itemIndex, 'set', e.target.value)}
                                       />
                                     <label>Repetitions:</label>
                                     <input
