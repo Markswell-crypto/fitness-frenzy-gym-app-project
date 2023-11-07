96fe5ef88cd5309e2242632aba0b1d408136963f
neville777 <neville.james@student.moringaschool.com>
2023-11-07 12:29:58 +0300

    Adding comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -12,10 +12,25 @@ const Comments = ({
     const [expand, setExpand] = useState(false);
     const inputRef = useRef(null);
   
-    useEffect(() => {
-      inputRef?.current?.focus();
-    }, [editMode]);
-    const onAddComment = () => {}
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, [editMode]);
+
+  const handleNewComment = () => {
+    setExpand(!expand);
+    setShowInput(true);
+  };
+    const onAddComment = () => {
+        if (editMode) {
+            handleEditNode(comment.id, inputRef?.current?.innerText);
+        } else {
+            setExpand(true);
+            handleInsertNode(comment.id, input);
+            setShowInput(false);
+            setInput("");
+        }
+    }
   return (
     <div>
         <div>
