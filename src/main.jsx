da790f9d5b29d421f314c7db1cce1af22cc8e810
neville777 <neville.james@student.moringaschool.com>
2023-11-08 15:07:00 +0300

    A correction on fetch backticks

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 
-import ReviewPage from './ReviewPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
