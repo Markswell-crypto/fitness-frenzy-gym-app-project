cc209746473993264cc43245677796cf2713433e
neville777 <neville.james@student.moringaschool.com>
2023-11-08 04:59:31 +0300

    Update the ReviewPage component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReviewPage from './component/ReviewPage.jsx'
+import ReviewPage from './ReviewPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
