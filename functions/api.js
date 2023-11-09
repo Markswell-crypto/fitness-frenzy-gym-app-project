ecb1ec32b5fec63269133ff22fab78115a002f05
Markswell <osoromarkswell80@gmail.com>
2023-11-10 00:43:59 +0300

    add cors

--- functions/api.js
+++ functions/api.js
@@ -5,11 +5,20 @@ const serverless = require('serverless-http');
 const jsonServer = require('json-server');
 
 const app = express();
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'YOUR_FRONTEND_DOMAIN');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
-app.use('/.netlify/functions/api', middlewares); 
-app.use('/.netlify/functions/api', router); 
+// Apply middleware to specific routes
+app.use('/.netlify/functions/api', middlewares);
+app.use('/.netlify/functions/api', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
